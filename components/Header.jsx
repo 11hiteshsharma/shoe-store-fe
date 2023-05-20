52689b4b717f463a3f7a303637eca10099b20d93
hiteshsharma <11hiteshsharma@gmail.com>
2023-05-20 14:42:06 +0530

    test-1

--- components/Header.jsx
+++ components/Header.jsx
@@ -1,7 +1,20 @@
 import React from "react";
-
+import { useState, useEffect } from "react";
+import Wrapper from "./Wrapper";
 const Header = () => {
-    return <div className="text">Header</div>;
+  const [mobileMenu, setMobileMenu] = useState(false);
+  const [showCatMenu, setShowCatMenu] = useState(false);
+  const [show, setShow] = useState("translate-y-0");
+  const [lastScrollY, setLastScrollY] = useState(0);
+  return (
+    <header
+      className={`w-full h-[50px] md:h-[80px] bg-white flex items-center justify-between z-20 sticky top-0 transition-transform duration-300 ${show}`}
+    >
+      <Wrapper>
+        <img src="/logo.svg" alt="" className="w-[40px] md:w-[60px]" />
+      </Wrapper>
+    </header>
+  );
 };
 
 export default Header;
